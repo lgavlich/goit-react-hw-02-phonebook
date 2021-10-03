fdbe1fbfd1bc61a65da0711395b55ecf88ad5fab
lgavlich <lgavlich@gmail.com>
2021-10-03 22:05:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Container from "./components/Container/Container";
+import Input from "./components/ContactList/ContactList";
+import Filter from "./components/Filter/Filter";
+import PhonebookList from "./components/PhonebookList/PhonebookList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+
+  formSubmit = (data) => {
+    console.log(data);
+    const { contacts } = this.state;
+    const added = contacts.some((contact) => contact.name === data.name);
+    if (added) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+
+    const contactData = {
+      id: uuidv4(),
+      name: data.name,
+      number: data.number,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [contactData, ...contacts],
+    }));
+  };
+
+  deleteList = (listId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== listId),
+    }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <Input onFormSubmit={this.formSubmit} />
+        <Filter value={filter} onChangeFilter={this.changeFilter} />
+        <PhonebookList
+          contacts={visibleContacts}
+          title="Contacts"
+          onDeleteList={this.deleteList}
+        />
+      </Container>
+    );
+  }
 }
 
 export default App;
